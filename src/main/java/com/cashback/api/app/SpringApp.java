1eac82237074ad21fb9afea37f2c34df71695c85
Georgi Yolovski <bluef0rever@abv.bg>
2017-05-08 15:34:13 +0300

    close qCB6hjU9 - Updated security configuration

--- src/main/java/com/cashback/api/app/SpringApp.java
+++ src/main/java/com/cashback/api/app/SpringApp.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.cashback.api" })
 @EnableJpaRepositories(basePackages = { "com.cashback.api.repositories" })
+@EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
 public class SpringApp {
 
 	public static void main(String[] args) {
